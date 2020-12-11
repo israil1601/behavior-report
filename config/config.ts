49b3752e34fe0e6e8cecbf7dcf98be95745e37a2
Isroilkhon Salikhodjaev <isroilkhon.salikhodjaev@aalto.fi>
2020-12-11 21:27:08 +0200

    add env

--- config/config.ts
+++ config/config.ts
@@ -5,17 +5,12 @@ if (Deno.env.get('DATABASE_URL')) {
   config.database = Deno.env.get('DATABASE_URL');
 } else {
   config.database = {
-    hostname: "hattie.db.elephantsql.com",
-    database: "ikanzwrs",
-    user: "ikanzwrs",
-    password: "SJjH7w4U23GT-zMWUV4tm2Rc64R8WIpr",
-    port: 5432
+    database: Deno.env.get("PGDATABASE"),
+    hostanme: Deno.env.get("PGHOST"),
+    user: Deno.env.get("PGUSER"),
+    password: Deno.env.get("PGPASSWORD"),
+    port: Deno.env.get("PGPORT")
   };
 }
 
-// database: Deno.env.get("PGDATABASE"),
-// hostanme: Deno.env.get("PGHOST"),
-// user: Deno.env.get("PGUSER"),
-// password: Deno.env.get("PGPASSWORD"),
-// port: Deno.env.get("PGPORT")
 export { config };
